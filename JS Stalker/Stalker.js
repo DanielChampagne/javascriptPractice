6ae01328e8bf8a3cd06f2f2dc0fd8e8267a65201
Daniel Champagne <danchampagne@hotmail.com>
2020-04-09 10:40:55 -0400

    Initial Commit

---
+++ JS Stalker/Stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Hello, what is your first name?");
+var lastName = prompt("Ok, what is your last name?");
+var age = prompt("How old are you?");
+
+console.log("Hello " + firstName + " " + lastName);
+console.log("Wow! You're " + age + " years old?!");
